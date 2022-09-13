52e9390fbe6dc304d6d0bd7c2fff5f4ba3dde844
Luana Trevizani <luanaftrevizani@gmail.com>
2022-09-13 17:09:08 -0300

    exercicios corrigidos

---
+++ exercicio2/script.js
@@ -0,0 +1,25 @@
+// # Exercício 2
+
+// Crie um programa que peça um input de número para o usuário. Com este número, o código deve imprimir a **tabuada** do número, de 1 a 10.
+numero = Number(prompt('Digite um número:'))
+tabuada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let i in tabuada) {
+  console.log(`${numero} x ${tabuada[i]} = ${numero * tabuada[i]}`)
+}
+
+// **Utilize o for...in para resolver**
+
+// Exemplo com entrada **`7`**:
+// ```jsx
+// 7
+// 14
+// 21
+// 28
+// 35
+// 42
+// 49
+// 56
+// 63
+// 70
+// ```
